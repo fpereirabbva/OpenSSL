0167df9bf071dbb68141693c2665cf385f8d8276
fernando.gabriel <>
2020-04-08 14:21:54 +0200

    Revert "rework"
    This reverts commit de7d4eea21da5e275cf91f808c2f0e120d45dbdf.

--- Frameworks/ios/openssl.framework/Headers/opensslconf.h
+++ Frameworks/ios/openssl.framework/Headers/opensslconf.h
@@ -1,31 +1,22 @@
-/*
- * Building OpenSSL for the different architectures of all iOS and tvOS devices requires different settings.
- * In order to be able to use assembly code on all devices, the choice was made to keep optimal settings for all
- * devices and use this intermediate header file to use the proper opensslconf.h file for each architecture.
 
- * See also https://github.com/x2on/OpenSSL-for-iPhone/issues/126 and referenced pull requests
- */
-
-#include <TargetConditionals.h>
-
-#if TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
-# include <openssl/opensslconf_ios_x86_64.h>
-#elif TARGET_OS_IOS && TARGET_OS_SIMULATOR && TARGET_CPU_X86
-# include <openssl/opensslconf_ios_i386.h>
-#elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM64
-# include <openssl/opensslconf_ios_arm64.h>
-#elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM && defined(__ARM_ARCH_7S__)
-# include <openssl/opensslconf_ios_armv7s.h>
-#elif TARGET_OS_IOS && TARGET_OS_EMBEDDED && TARGET_CPU_ARM && !defined(__ARM_ARCH_7S__)
-# include <openssl/opensslconf_ios_armv7.h>
-#elif TARGET_OS_TV && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
-# include <openssl/opensslconf_tvos_x86_64.h>
-#elif TARGET_OS_TV && TARGET_OS_EMBEDDED && TARGET_CPU_ARM64
-# include <openssl/opensslconf_tvos_arm64.h>
-#elif TARGET_OS_OSX && TARGET_CPU_X86_64
-# include <openssl/opensslconf_macos_x86_64.h>
-#elif TARGET_OS_OSX && TARGET_CPU_X86
-# include <openssl/opensslconf_macos_i386.h>
-#else
-# error Unable to determine target or target not included in OpenSSL build
+/* opensslconf.h */
+#if defined(__APPLE__) && defined (__x86_64__)
+# include <openssl/opensslconf-x86_64.h>
 #endif
+
+#if defined(__APPLE__) && defined (__i386__)
+# include <openssl/opensslconf-i386.h>
+#endif
+
+#if defined(__APPLE__) && defined (__arm__) && defined (__ARM_ARCH_7A__)
+# include <openssl/opensslconf-armv7.h>
+#endif
+
+#if defined(__APPLE__) && defined (__arm__) && defined (__ARM_ARCH_7S__)
+# include <openssl/opensslconf-armv7s.h>
+#endif
+
+#if defined(__APPLE__) && (defined (__arm64__) || defined (__aarch64__))
+# include <openssl/opensslconf-arm64.h>
+#endif
+
